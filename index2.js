c78673146e7bcbdd6fe7d5d71c2d4f0b1f8cab5f
leonardoschroeder <leonardoschroeder@hotmail.com>
2018-03-20 18:00:22 -0300

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,36 @@
+var http = require('http');
+var crypto = require("crypto")
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+	var key = "supersecretkey";
+    var text = "testandocriptografia";
+    var encryptedText = encrypt(key, text);
+    var decryptedText = decrypt(key, encryptedText);
+	
+	response.end("Hello World! || cript:" + encryptedText + "descript:" + decryptedText + ".");
+
+});
+
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
+
+function encrypt(key, data) {
+	var cipher = crypto.createCipher('aes-256-cbc', key);
+	var crypted = cipher.update(text, 'utf-8', 'hex');
+	crypted += cipher.final('hex');
+
+	return crypted;
+}
+
+function decrypt(key, data) {
+	var decipher = crypto.createDecipher('aes-256-cbc', key);
+	var decrypted = decipher.update(data, 'hex', 'utf-8');
+	decrypted += decipher.final('utf-8');
+
+	return decrypted;
+}
+	
