4e2a63f8169f5882182a8a2ee0f5e19f78a29b13
Walter Silvestre Coan <walter.coan@gmail.com>
2018-03-20 20:29:26 -0300

    novo teste

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var crypto = require("crypto")
 
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+	var key = "supersecretkey";
+    var text = "testandocriptografia";
+    var encryptedText = encrypt(key, text);
+    var decryptedText = decrypt(key, encryptedText);
+	
+	response.end("Hello World! || cript:" + encryptedText + "descript:" + decryptedText + ".");
 
 });
 
@@ -11,3 +17,20 @@ var port = process.env.PORT || 1337;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
+
+function encrypt(key, data) {
+	var cipher = crypto.createCipher('aes-256-cbc', key);
+	var crypted = cipher.update(text, 'utf-8', 'hex');
+	crypted += cipher.final('hex');
+
+	return crypted;
+}
+
+function decrypt(key, data) {
+	var decipher = crypto.createDecipher('aes-256-cbc', key);
+	var decrypted = decipher.update(data, 'hex', 'utf-8');
+	decrypted += decipher.final('utf-8');
+
+	return decrypted;
+}
+	
