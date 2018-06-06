b902f85ca2fe9c1643ddfce367a81d14b1c5327b
leonardoschroeder <leonardoschroeder@hotmail.com>
2018-06-06 16:05:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,20 +7,16 @@ var server = http.createServer(function(request, response) {
     var encryptedText = encrypt(key, text);
     var decryptedText = decrypt(key, encryptedText);
 	
-	
 	function encrypt(key, data) {
 		var cipher = crypto.createCipher('aes-256-cbc', key);
 		var crypted = cipher.update(text, 'utf-8', 'hex');
 		crypted += cipher.final('hex');
-
 		return crypted;
 	}
-
 	function decrypt(key, data) {
 		var decipher = crypto.createDecipher('aes-256-cbc', key);
 		var decrypted = decipher.update(data, 'hex', 'utf-8');
 		decrypted += decipher.final('utf-8');
-
 		return decrypted;
 	}
 	response.writeHead(200, {"Content-Type": "text/plain"});
